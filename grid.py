7c0fa39cf2e5e24f3edd299f1c8e8febcaa31070
tjeckleburg <tjeckleburg@users.noreply.github.com>
2017-12-04 18:15:38 -0800

    Improvements on Curve and Grid class inits

--- grid.py
+++ grid.py
@@ -1,56 +1,70 @@
 import scipy as sp
 
-
 class Curve:
-''' Curve data type and operations '''
+    ''' Curve data type and operations '''
 
-	def __init__(self,np=11,feat=None,func=None):
-		self.np = np
+    def __init__(self,definition=None,np=11,feature=False):
+        self.np = np
 
-		if feat is True: #Is a feature curve
-			self.feat = True
-		else:
-			self.feat = False
+        if definition is None:
+            #Make up one based on default values 
+            np = self.np
+            x = sp.linspace(0,1,np)
+            y = sp.zeros_like(x)
+            z = sp.zeros_like(x)
+            self.defCurveXYZ = sp.concatenate((x,y,z))
+        # elif type(definition) is list or numpy.ndarray:
+        # curve is defined as a set of discrete points. Use them. ignore np. Or interpolate?
+        # elif type(definition) is string
+        # curve is defined by a python expressions t=[0,1]; x=x(t); y=y(t); z=z(t). Eval. it
+        # tip: Consider using x,y,z,=t as the default when definition==None. 
 
-		if func is None: #Is not defined by a math function. 
-			self.func = None
-			pass
-		else:
-			np = self.np
-			x = sp.linspace(0,1,np)
-			y = sp.zero_like(x)
-			z = sp.zeros_like(x)
-			defCurve = sp.vstack(x,y,z)
+        if feature is True: #This is a feature curve
+            self.feat = True
+            #Consider this as the best definition available.
+        else:
+            self.feat = False
 
-
-
-
-
+    def __str__(self):
+        sp.set_printoptions(precision=8)
+        return self.defCurveXYZ.__str__()
 
 class Grid:
-'''structured grid data and operations.'''
+    '''structured grid data and operations.'''
 
-	gridCount = 0
+    totalGridCount = 0
 
-	def __init__(self, jmax=11, kmax=1, lmax=1):
-		sGrid.gridCount += 1
-		self.jmax = jmax
-		self.KminCurve = None
+    def __init__(self, jmax=11, kmax=1, lmax=1):
+        Grid.totalGridCount += 1
+        self.gridNumber = Grid.totalGridCount
+        #Maximum indices in each direction. Minimum is 1 for all.
+        self.jmax_ind=jmax
+        self.kmax_ind=kmax
+        self.lmax_ind=lmax
+        
+        #Curves that bound a JK-surface grid
+        self.bnd_cur = ('jmin_cur', 'jmax_cur', 'kmin_cur', 'kmax_cur')
 
-	def initKminCurve(self,defCurve=None):
-		if defCurve is None:
+        for attr in self.bnd_cur:
+            setattr(self,attr,None)
 
-	
-		self.KminCurve = defCurve
+    def defineBoundary(self,boundary=None,definition=None):
 
-	def redist(self,curve,ds1=0.1,sr=1.2,ds2=0.2,n=11):
-		ksi = sp.linspace(0,1,n)
-		s  = 1 + sp.tanh(sr*(ksi-1))/sp.tanh(sr)
-		self.KminCurve = s*(curve[-1]-curve[0]) + curve[0]
+        if boundary in self.bnd_cur:
+            if getattr(self,boundary):
+                print("WARNING: {0:%s} boundary for grid {1:%d} is already defined.".format(self.boundary,self.gridNumber))
+            setattr(self,boundary,Curve())
+        else:
+            print("ERROR: The boundary must be specified as one of these: %s".format(self.bnd_cur))
+
+    def redist(self,curve,ds1=0.1,sr=1.2,ds2=0.2,n=11):
+        ksi = sp.linspace(0,1,n)
+        s  = 1 + sp.tanh(sr*(ksi-1))/sp.tanh(sr)
+        curve_redist = s*(curve[-1]-curve[0]) + curve[0]
 
 
 
-		
+        
 
 
 
--- makeGrids.py
+++ makeGrids.py
@@ -3,17 +3,11 @@ import scipy as sp
 from matplotlib import pyplot as plt
 import grid  as grid
 
+grid1 = grid.Grid()
 
-grid1 = grid.sGrid()
+print(grid1.jmax_ind)
 
-print(grid1.jmax)
+grid1.defineBoundary(boundary='kmin_cur')
 
-grid1.initKminCurve()
-
-print(grid1.KminCurve)
-
-grid1.redist()
-
-print(grid1.KminCurve)
-
+print(grid1.kmin_cur)
 
